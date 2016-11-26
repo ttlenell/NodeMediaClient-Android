e2dfd29b389c676b460731c107894861c90edd73
Mingliang Chen <illuspas@gmail.com>
2016-11-26 22:36:54 +0800

    update to v1.3.1

--- app/src/main/java/cn/nodemedia/nodemediaclient/NodeAVPlayerDemoActivity.java
+++ app/src/main/java/cn/nodemedia/nodemediaclient/NodeAVPlayerDemoActivity.java
@@ -38,15 +38,31 @@ public class NodeAVPlayerDemoActivity extends AppCompatActivity implements Media
             }
         });
         nvp.setSurfaceView(videoSurface,NodeAVPlayer.UIViewContentModeScaleAspectFit);
+
+        /**
+         * 开启H.264硬解码
+         * 实验功能,因Android碎片化,兼容性并不保证.若初始化失败自动转软解,但存在部分机型初始化成功但解码失败或跳帧或绿边的情况
+         */
         nvp.setHWEnable(true);
 
         String url = SharedPreUtil.getString(this,"avPlayUrl");
+
+        /**
+         * 设置视频源,可以是http的web地址,也可以是本地文件路径
+         */
         nvp.setDataSource(url);
+
+        /**
+         * 开始播放
+         */
         nvp.start();
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+        /**
+         * 点击显示区域显示控制条
+         */
         mediaController.show();
         return false;
     }
@@ -67,31 +83,47 @@ public class NodeAVPlayerDemoActivity extends AppCompatActivity implements Media
     @Override
     protected void onResume() {
         super.onResume();
+
         nvp.start();
     }
 
     @Override
     public void start() {
+        /**
+         * 响应控制条的播放操作,如被暂停,则继续播放
+         */
         nvp.start();
     }
 
     @Override
     public void pause() {
+        /**
+         * 响应控制条的暂停操作
+         */
         nvp.pause();
     }
 
     @Override
     public int getDuration() {
+        /**
+         * 获取视频总时长
+         */
         return nvp.getDuration();
     }
 
     @Override
     public int getCurrentPosition() {
+        /**
+         * 获取视频当前位置
+         */
         return nvp.getCurrentPosition();
     }
 
     @Override
     public void seekTo(int pos) {
+        /**
+         * 定位播放进度到多少秒处
+         */
         nvp.seekTo(pos);
     }
 
@@ -102,6 +134,9 @@ public class NodeAVPlayerDemoActivity extends AppCompatActivity implements Media
 
     @Override
     public int getBufferPercentage() {
+        /**
+         * 获取缓冲垫
+         */
         return nvp.getBufferPercentage();
     }
 
